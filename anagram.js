5afd4f97e6dc11117151e5b2e309638f20032996
edwinbodge <bodgee95@gmail.com>
2016-10-04 13:57:40 -0400

    Update anagram.js

--- anagram.js
+++ anagram.js
@@ -2,6 +2,6 @@ function myFunction() {
     var x = document.getElementById("frm1");
     var myword1 = x.elements[0].value;
     var myword2 = x.elements[1].value;
-    document.getElementById("demo1").innerHTML = myword1;
+    document.getElementById("demo1").innerHTML = "are you working??";
     document.getElementById("demo2").innerHTML = myword2;
 }
