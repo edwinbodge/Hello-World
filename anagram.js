e9d607b09449d698fd6b0929e9b2976a4bb7307a
edwinbodge <bodgee95@gmail.com>
2016-10-04 14:22:37 -0400

    Update anagram.js

--- anagram.js
+++ anagram.js
@@ -5,3 +5,20 @@ function myFunction() {
     document.getElementById("demo1").innerHTML = myword1;
     document.getElementById("demo2").innerHTML = myword2;
 }
+
+function checkAnagram() {
+    var x = document.getElementById("frm1");
+    var word1 = x.elements[0].value;
+    var word2 = x.elements[1].value;
+    // var regex = /[^a-z0-9]/gi;
+    var sortedWord1 = function sortMyString(word1);
+    var sortedWord2 = sortMyString(word2);
+    var answer = sortedWord1 === sortedWord2;
+    document.getElementById("demo1").innerHTML = "Something happened!";
+    document.getElementById("demo2").innerHTML = answer;
+}
+
+function sortMyString(str) {
+    var regex = /[^a-z0-9]/gi;
+    return str.replace(regex,'').toLowerCase().split('').sort().join('');
+}
