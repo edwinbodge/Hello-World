5f91046ee039fb653cd12104f75331dc370bb005
edwinbodge <bodgee95@gmail.com>
2016-10-04 14:28:55 -0400

    Update anagram.js

--- anagram.js
+++ anagram.js
@@ -1,24 +1,14 @@
-function myFunction() {
-    var x = document.getElementById("frm1");
-    var myword1 = x.elements[0].value;
-    var myword2 = x.elements[1].value;
-    document.getElementById("demo1").innerHTML = myword1;
-    document.getElementById("demo2").innerHTML = myword2;
-}
-
 function checkAnagram() {
     var x = document.getElementById("frm1");
     var word1 = x.elements[0].value;
     var word2 = x.elements[1].value;
-    // var regex = /[^a-z0-9]/gi;
     var sortedWord1 = sortMyString(word1);
     var sortedWord2 = sortMyString(word2);
     var answer = sortedWord1 === sortedWord2;
-    document.getElementById("demo1").innerHTML = "Something happened!";
-    document.getElementById("demo2").innerHTML = answer;
+    document.getElementById("demo1").innerHTML = answer;
 }
 
 function sortMyString(str) {
-    var regex = /[^a-z0-9]/gi;
-    return str.replace(regex,'').toLowerCase().split('').sort().join('');
+    var keep = /[^a-z0-9]/gi;
+    return str.replace(keep,'').toLowerCase().split('').sort().join('');
 }
