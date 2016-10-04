dcf65f4166b37e95a9ff0a45818a7908433e3a94
edwinbodge <bodgee95@gmail.com>
2016-10-04 19:09:43 -0400

    Update anagram.js

--- anagram.js
+++ anagram.js
@@ -1,14 +1,43 @@
-function checkAnagram() {
-    var x = document.getElementById("frm1");
-    var word1 = x.elements[0].value;
-    var word2 = x.elements[1].value;
-    var sortedWord1 = sortMyString(word1);
-    var sortedWord2 = sortMyString(word2);
-//    var answer = sortedWord1 === sortedWord2;
-    document.getElementById("demo1").innerHTML = (sortedWord1 === sortedWord2);
-}
+$('.form').find('input, textarea').on('keyup blur focus', function (e) {
+  
+  var $this = $(this),
+      label = $this.prev('label');
 
-function sortMyString(str) {
-    var keep = /[^a-z0-9]/gi;
-    return str.replace(keep,'').toLowerCase().split('').sort().join('');
-}
+	  if (e.type === 'keyup') {
+			if ($this.val() === '') {
+          label.removeClass('active highlight');
+        } else {
+          label.addClass('active highlight');
+        }
+    } else if (e.type === 'blur') {
+    	if( $this.val() === '' ) {
+    		label.removeClass('active highlight'); 
+			} else {
+		    label.removeClass('highlight');   
+			}   
+    } else if (e.type === 'focus') {
+      
+      if( $this.val() === '' ) {
+    		label.removeClass('highlight'); 
+			} 
+      else if( $this.val() !== '' ) {
+		    label.addClass('highlight');
+			}
+    }
+
+});
+
+$('.tab a').on('click', function (e) {
+  
+  e.preventDefault();
+  
+  $(this).parent().addClass('active');
+  $(this).parent().siblings().removeClass('active');
+  
+  target = $(this).attr('href');
+
+  $('.tab-content > div').not(target).hide();
+  
+  $(target).fadeIn(600);
+  
+});
